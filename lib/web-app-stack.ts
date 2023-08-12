cf3eec6e0651a2ac9798f47f15d123c631fd86db
namdev-rathod <namdev.devops@gmail.com>
2023-08-13 02:42:13 +0530

    web server working code

--- lib/web-app-stack.ts
+++ lib/web-app-stack.ts
@@ -11,6 +11,13 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import * as elasticloadbalancingv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as elasticloadbalancingv2targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+
+
+// export class MyLoadBalancerStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
 
 
 export class WebAppStack extends cdk.Stack {
@@ -24,6 +31,8 @@ export class WebAppStack extends cdk.Stack {
 
         // Create a new VPC with two public subnets (one in each AZ) and CIDR 10.0.0.0/16
         const vpc = new Vpc(this, 'MyVpc', {
+          cidr: '10.0.0.0/16',
+          vpcName: 'webapp-vpc',
           maxAzs: 2,
           subnetConfiguration: [
             {
@@ -33,8 +42,9 @@ export class WebAppStack extends cdk.Stack {
           ],
         });
 
-        // Create VPC and two public subnets in different availability zones
+      //  Create VPC and two public subnets in different availability zones
         // const vpc = new ec2.Vpc(this, 'MyVPC', {
+        //   vpcName: 'webapp-vpc',
         //   cidr: '10.0.0.0/16',
         //   maxAzs: 2,
         //   subnetConfiguration: [
@@ -52,41 +62,41 @@ export class WebAppStack extends cdk.Stack {
         // });
 
     //  create Security Group for the Instance
-    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
-      securityGroupName: ('web-server-poc-sg'),
-      vpc,
-      allowAllOutbound: true,
-    });
+      const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+        securityGroupName: ('web-server-poc-sg'),
+        vpc,
+        allowAllOutbound: true,
+     });
 
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      'allow SSH access from anywhere',
-    );
+      webserverSG.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(22),
+        'allow SSH access from anywhere',
+     );
 
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'allow HTTP traffic from anywhere',
-    );
+      webserverSG.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(80),
+        'allow HTTP traffic from anywhere',
+      );
 
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      'allow HTTPS traffic from anywhere',
-    );
+      webserverSG.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(443),
+        'allow HTTPS traffic from anywhere',
+      );
 
-    webserverSG.addIngressRule(
-      ec2.Peer.ipv4("192.168.10.10/32"),
-      ec2.Port.tcp(3309),
-      'allow RDP traffic from anywhere',
-    );
+      webserverSG.addIngressRule(
+        ec2.Peer.ipv4("192.168.10.10/32"),
+        ec2.Port.tcp(3309),
+        'allow RDP traffic from anywhere',
+      );
 
     // Create EBS volume with custom size
-    const rootVolume: ec2.BlockDevice = {
-      deviceName: '/dev/xvda', // Use the root device name from Step 1
-      volume: ec2.BlockDeviceVolume.ebs(15), // Override the volume size in Gibibytes (GiB)
-    };
+      const rootVolume: ec2.BlockDevice = {
+        deviceName: '/dev/xvda', // Use the root device name from Step 1
+        volume: ec2.BlockDeviceVolume.ebs(15), // Override the volume size in Gibibytes (GiB)
+      };
 
 
     // Create EC2 instances in different availability zones
@@ -124,52 +134,90 @@ export class WebAppStack extends cdk.Stack {
         });
 
     // load contents of script
-    const userDataScript1 = readFileSync('./lib/user-data1.sh', 'utf8');
-    const userDataScript2 = readFileSync('./lib/user-data2.sh', 'utf8');
+      const userDataScript1 = readFileSync('./lib/user-data1.sh', 'utf8');
+      const userDataScript2 = readFileSync('./lib/user-data2.sh', 'utf8');
     // add the User Data script to the Instance
-    ec2Instance1.addUserData(userDataScript1);
-    ec2Instance2.addUserData(userDataScript2);
+      ec2Instance1.addUserData(userDataScript1);
+      ec2Instance2.addUserData(userDataScript2);
 
     // Create an Application Load Balancer
-    const alb = new ApplicationLoadBalancer(this, 'webapplb', {
-      loadBalancerName: 'webapp-alb',
+        const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'MyLoadBalancer', {
+          loadBalancerName: 'webapp-alb',
+          vpc,
+          securityGroup: webserverSG,
+          internetFacing: true, // Set to false for internal load balancer
+          http2Enabled: true,
+            vpcSubnets: {
+              subnetType: ec2.SubnetType.PUBLIC, // Choose the appropriate subnet type
+              availabilityZones: ['ap-south-1a', 'ap-south-1b'], // Specify the desired availability zone
+            },
+         });
+
+
+    // Create a Target Group with HTTP listener
+    const TargetGroup = new elbv2.ApplicationTargetGroup(this, 'HttpTargetGroup', {
+      targetGroupName: 'webapp-tg',
       vpc,
-      internetFacing: true,
-      securityGroup: webserverSG,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC, // Choose the appropriate subnet type
-        availabilityZones: ['ap-south-1a', 'ap-south-1b'], // Specify the desired availability zone
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      port: 80,
+      targetType: elbv2.TargetType.INSTANCE,
+      healthCheck: {
+        path: '/',
+        protocol: elbv2.Protocol.HTTP,
       },
     });
 
+         // TargetGroup.addTarget(ec2Instance1);
+         // TargetGroup.addTarget(ec2Instance2);
+      
+          // Create an HTTP listener
+        //   const httpListener = loadBalancer.addListener('HttpListener', {
+        //     port: 80,
+        //     defaultTargetGroups: [TargetGroup],
+        //   });
 
-   // Create a target group for the instances
-        const targetGroup = new cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup(this, 'InstanceTargetGroup', {
-          port: 80,
-          vpc,
-          targetType: elbv2.TargetType.INSTANCE,
-          healthCheck: {
-            path: '/',
-            protocol: elbv2.Protocol.HTTP,
-          },
+
+        // Create an HTTPS listener
+        const certificateArn = 'arn:aws:acm:ap-south-1:666930281169:certificate/61427c62-07d7-45c8-b46f-c5fbac653c4a'; // Replace with your ACM certificate ARN
+        const httpsListener = loadBalancer.addListener('HttpsListener', {
+          port: 443,
+          certificates: [elbv2.ListenerCertificate.fromArn(certificateArn)],
+          defaultTargetGroups: [TargetGroup],
         });
 
-    // Add SSL certificate ARN from ACM
-          const certificateArn = 'arn:aws:acm:ap-south-1:666930281169:certificate/61427c62-07d7-45c8-b46f-c5fbac653c4a';
-          const listener = alb.addListener('HTTPSListener', {
-            port: 443,
-            protocol: elbv2.ApplicationProtocol.HTTPS,
-            defaultTargetGroups: [targetGroup],
-            certificates: [elbv2.ListenerCertificate.fromArn(certificateArn)],
-          });
+      // // Create an HTTP listener to redirect to HTTPS
+         const httpListener = loadBalancer.addListener('HttpListener', {
+           port: 80,
+           defaultAction: elbv2.ListenerAction.redirect({
+           protocol: 'HTTPS',
+           port: '443',
+           }),
+         });
 
-    // Attach the target group to the ALB listener
-    //       listener.addTargets(
-    //         'InstanceTarget',
-    //             {
-    //       port: 80,
-    //       targets: [instance2],
-    // });
+      // Get an existing hosted zone (replace 'YOUR_HOSTED_ZONE_ID' with actual ID)
+        const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'ExistingHostedZone', {
+          hostedZoneId: 'Z0808885ROJ6FHJCCCXN',
+          zoneName: 'awsguruji.net',
+        });
 
-  }
-}
+      // Create a DNS record for the load balancer
+        new route53.ARecord(this, 'WebAppDNSRecord', {
+          zone: hostedZone,
+          recordName: 'web',
+          target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(loadBalancer)),
+        });
+
+        // Output the DNS name of the load balancer
+          new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+          value: loadBalancer.loadBalancerDnsName,
+        });
+
+      }
+    }
+    
+
+
+
+
+
+
